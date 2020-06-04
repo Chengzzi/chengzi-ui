09fda012e77769d6d558177643155ace7b78c65c
Chengzzi <360382942@qq.com>
2020-06-04 14:23:46 +0800

    修改icon class

--- template/icon/icon.vue
+++ template/icon/icon.vue
@@ -1,8 +1,10 @@
 <template>
-    <i :class="classes" v-if="!name.includes('color')" v-show="name"></i>
-    <svg :class="classes" v-else v-show="name">
-        <use :xlink:href="`#i-${name}`"></use>
-    </svg>
+    <div class="cz-icon" @click="$emit('click')">
+        <i :class="classes" v-if="!name.includes('color')" v-show="name"></i>
+        <svg :class="classes" v-else v-show="name">
+            <use :xlink:href="`#i-${name}`"></use>
+        </svg>
+    </div>
 </template>
 <script>
     export default {
@@ -19,7 +21,6 @@
         computed: {
             classes(){
                 return{
-                    icon:true,
                     colorfont:this.name.includes("color"),
                     iconfont:!this.name.includes("color"),
                     [`i-${this.name}`]:!this.name.includes("color"),
@@ -31,41 +32,44 @@
 </script>
 
 <style scoped>
-    .colorfont{
-        width: 1em;
-        height: 1em;
-    }
-    .colorfont.logo{
-        width: 2em;
-        height: 2em;
-    }
-    .colorfont.large{
-        width: 1.5em;
-        height: 1.5em;
-    }
-    .colorfont.small{
-        width: 0.8em;
-        height: 0.8em;
-    }
+.cz-icon{
+    display: inline-block;
+}
+.colorfont{
+    width: 1em;
+    height: 1em;
+}
+.colorfont.logo{
+    width: 2em;
+    height: 2em;
+}
+.colorfont.large{
+    width: 1.5em;
+    height: 1.5em;
+}
+.colorfont.small{
+    width: 0.8em;
+    height: 0.8em;
+}
 
-    @keyframes sbin {
-       0%{transform:rotate(0deg)}
-       100%{transform:rotate(360deg)}
-    }
-    .iconfont.i-loading{
-        animation: sbin 1s linear infinite;
-    }
-    .iconfont{
-        display: inline-block;
-        font-size: 1em;
-    }
-    .iconfont.logo{
-        font-size: 2em;
-    }
-    .iconfont.large{
-        font-size: 1.5em;
-    }
-    .iconfont.small{
-        font-size: 0.8em;
-    }
+@keyframes sbin {
+   0%{transform:rotate(0deg)}
+   100%{transform:rotate(360deg)}
+}
+.iconfont.i-loading{
+    animation: sbin 1s linear infinite;
+}
+.iconfont{
+    display: inline-block;
+    font-size: 1em;
+}
+.iconfont.logo{
+    font-size: 2em;
+}
+.iconfont.large{
+    font-size: 1.5em;
+}
+.iconfont.small{
+    font-size: 0.8em;
+}
 </style>
